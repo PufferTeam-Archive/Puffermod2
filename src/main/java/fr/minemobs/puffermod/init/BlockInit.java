8b81211884374f0647463a8aff50b4ca1c6e7967
minemobs <issahakim94@gmail.com>
2020-12-21 00:03:45 +0100

    Patched x-ray glitch

--- src/main/java/fr/minemobs/puffermod/init/BlockInit.java
+++ src/main/java/fr/minemobs/puffermod/init/BlockInit.java
@@ -2,6 +2,7 @@ package fr.minemobs.puffermod.init;
 
 import fr.minemobs.puffermod.Puffermod;
 import fr.minemobs.puffermod.object.block.WoodenFrameBlock;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.material.Material;
@@ -21,5 +22,5 @@ public class BlockInit {
             Block.Properties.from(Blocks.REDSTONE_BLOCK).sound(WOOD)));
 
     public static final RegistryObject<Block> test_block = BLOCKS.register("test_block", () -> new Block(
-            Block.Properties.create(Material.IRON).hardnessAndResistance(0.5f, 15.0f).sound(METAL)));
+            Block.Properties.create(Material.IRON).hardnessAndResistance(0.5f, 15.0f).sound(METAL).notSolid().setOpaque(AbstractBlock.AbstractBlockState::isOpaqueCube)));
 }
